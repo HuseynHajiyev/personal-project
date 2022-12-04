9e0a8cb8a547b3597f327af9c9ae1391db914f18
HuseynHajiyev <hhajiyev@hotmail.com>
2022-12-05 02:00:42 +0400

    Internet speeds model added, place:foreign_key, download_units:string, download_speed:float

---
+++ db/migrate/20221204213418_create_internet_speeds.rb
@@ -0,0 +1,11 @@
+class CreateInternetSpeeds < ActiveRecord::Migration[7.0]
+  def change
+    create_table :internet_speeds, id: :uuid do |t|
+      t.references :place, null: false, foreign_key: true, index: true, type: :uuid
+      t.string :download_units, null: false, scale: 2, precision: 15
+      t.float :download_speed, null: false
+
+      t.timestamps
+    end
+  end
+end
