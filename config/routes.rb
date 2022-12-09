324f1a714473c3261a3ca93a64326e074b326482
HuseynHajiyev <hhajiyev@hotmail.com>
2022-12-10 00:25:19 +0400

    Changed places controller so that it returns relevant data as json and changed the routes for correct api hits

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,12 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   root 'places#index'
+
+  namespace :api do
+    namespace :v1 do
+      resources :places
+    end
+  end
+
+  get '*path', to: 'places#index', via: :all
 end
